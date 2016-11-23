36f2cf2fbbe4286c18f5c743412e4a97f8b243cf
Leo Burdianov <leo.burdianov@gmail.com>
2016-11-24 00:29:50 +0400

    Add Icons and Custom View in Collapsing Toolbar

--- app/src/main/java/com/testography/castermaterialdesign/MainActivity.java
+++ app/src/main/java/com/testography/castermaterialdesign/MainActivity.java
@@ -2,8 +2,14 @@ package com.testography.castermaterialdesign;
 
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +25,50 @@ public class MainActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle("Action Bar Title");
+
+        setCustomView(toolbar);
+    }
+
+    private void setCustomView(Toolbar toolbar) {
+        View customView = getLayoutInflater().inflate(R.layout.view_toolbar, null);
+        toolbar.addView(customView);
+
+        ImageView happyFace = (ImageView) customView.findViewById(R.id.toolbar_image);
+        happyFace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeImage();
+            }
+        });
+    }
+
+    private void changeImage() {
+        ImageView backdropIV = (ImageView) findViewById(R.id.backdrop);
+        backdropIV.setImageResource(R.drawable.hero_statues);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        CoordinatorLayout cl = (CoordinatorLayout) findViewById(R.id.main_content);
+        switch (item.getItemId()) {
+            case R.id.action_heart:
+                Snackbar.make(cl, "HEART HEART HEART HEART", Snackbar.LENGTH_LONG).show();
+                return true;
+            case R.id.action_star:
+                Snackbar.make(cl, "STAR STAR STAR STAR", Snackbar.LENGTH_LONG)
+                        .show();
+                return true;
+            case R.id.action_joystick:
+                Snackbar.make(cl, "JOYSTICK JOYSTICK JOYSTICK JOYSTICK", Snackbar
+                        .LENGTH_LONG).show();
+                return true;
+        }
+        return true;
     }
 }
